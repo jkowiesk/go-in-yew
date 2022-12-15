da33796ed599af5e54e3a1e1cd8a0622fd441636
Jakub Kowieski <01158762@pw.edu.pl>
2022-12-15 23:04:48 +0100

    Change server to be more readable

--- rust_go-server/src/main.rs
+++ rust_go-server/src/main.rs
@@ -3,43 +3,49 @@ use websocket::sync::Server;
 use websocket::OwnedMessage;
 
 fn main() {
-	let server = Server::bind("127.0.0.1:8888").unwrap();
+    let server = Server::bind("127.0.0.1:8888").expect("Failed to bind to server address");
 
-	for request in server.filter_map(Result::ok) {
-		thread::spawn(|| {
-			if !request.protocols().contains(&"rust-websocket".to_string()) {
-				request.reject().unwrap();
-				return;
-			}
+    for request in server.filter_map(Result::ok) {
+        thread::spawn(move || {
+            if !request.protocols().contains(&"rust-websocket".to_string()) {
+                request.reject().expect("Failed to reject request");
+                return;
+            }
 
-			let mut client = request.use_protocol("rust-websocket").accept().unwrap();
+            let mut client = request
+                .use_protocol("rust-websocket")
+                .accept()
+                .expect("Failed to accept websocket connection");
 
-			let ip = client.peer_addr().unwrap();
+            let ip = client.peer_addr().expect("Failed to get peer address");
+            println!("Connection from {}", ip);
 
-			println!("Connection from {}", ip);
+            let message = OwnedMessage::Text("Hello".to_string());
+            client.send_message(&message).expect("Failed to send message");
 
-			let message = OwnedMessage::Text("Hello".to_string());
-			client.send_message(&message).unwrap();
+            let (mut receiver, mut sender) = client.split().expect("Failed to split client");
 
-			let (mut receiver, mut sender) = client.split().unwrap();
+            for message in receiver.incoming_messages() {
+                let message = message.expect("Failed to receive message");
 
-			for message in receiver.incoming_messages() {
-				let message = message.unwrap();
-
-				match message {
-					OwnedMessage::Close(_) => {
-						let message = OwnedMessage::Close(None);
-						sender.send_message(&message).unwrap();
-						println!("Client {} disconnected", ip);
-						return;
-					}
-					OwnedMessage::Ping(ping) => {
-						let message = OwnedMessage::Pong(ping);
-						sender.send_message(&message).unwrap();
-					}
-					_ => sender.send_message(&message).unwrap(),
-				}
-			}
-		});
-	}
+                match message {
+                    OwnedMessage::Close(_) => {
+                        let message = OwnedMessage::Close(None);
+                        sender
+                            .send_message(&message)
+                            .expect("Failed to send close message");
+                        println!("Client {} disconnected", ip);
+                        return;
+                    }
+                    OwnedMessage::Ping(ping) => {
+                        let message = OwnedMessage::Pong(ping);
+                        sender
+                            .send_message(&message)
+                            .expect("Failed to send pong message");
+                    }
+                    _ => sender.send_message(&message).expect("Failed to send message"),
+                }
+            }
+        });
+    }
 }
