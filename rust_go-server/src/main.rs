a15cc34a24b1615f35028751300b118736a7be17
Kacper Klassa <kacperklassa@gmail.com>
2023-01-14 15:54:20 +0100

    fix player messaging order

--- rust_go-server/src/main.rs
+++ rust_go-server/src/main.rs
@@ -14,16 +14,13 @@ pub struct Game {
 }
 
 
-fn send_game_state(game: &MutexGuard<Game>, player: &Sender) {
+fn send_game_state(game: &MutexGuard<Game>, player: &Sender, your_turn: bool) {
     let board = game.board.clone();
-    let player_turn = game.player1_turn;
     let data = json!({ 
-        "first_player_turn": player_turn,
+        "your_turn": your_turn,
         "board": board 
     });
     player.send(Message::text(data.to_string())).unwrap();
-    // player.send(Message::binary(board)).unwrap();
-    // player.send(Message::text(format!("{}", player_turn))).unwrap();
 }
 
 fn main() {
@@ -43,40 +40,40 @@ fn main() {
             
             if game.player1.is_none() {
                 game.player1 = Some(out.clone());
-                send_game_state(&game, &out);
+                send_game_state(&game, &out, game.player1_turn);
+                println!("First player joined the game, id: {}", out.connection_id());
             } 
             else if game.player2.is_none() {
                 game.player2 = Some(out.clone());
-                send_game_state(&game, &out);
+                send_game_state(&game, &out, !game.player1_turn);
+                println!("Second player joined the game, id: {}", out.connection_id());
             }
             else {
-                println!("Received data from client: {}", msg);
+                println!("Received data from player {}: {}", out.connection_id(), msg);
     
                 let json_str: String = msg.into_text().unwrap();
                 let json_value: Value = serde_json::from_str(&json_str).unwrap();
                 let board: Vec<u8> = json_value["board"].as_array().unwrap().into_iter().map(|x| {
                     x.as_u64().unwrap() as u8
                 }).collect();
-                
-                println!("Previous board state: {:?}", game.board);
-                println!("Received board state: {:?}", board);
 
                 game.board = board;
                 println!("Updated game board state: {:?}", game.board);
                 
-                game.player1_turn = !game.player1_turn;
-    
-                send_game_state(&game, &game.player1.as_ref().unwrap());
-                send_game_state(&game, &game.player2.as_ref().unwrap());
-    
                 if game.player1_turn {
-                    game.player1.as_ref().unwrap().send(Message::text("Your turn")).unwrap();
                     println!("Player 1 moved!");
+                    game.player2.as_ref().unwrap().send(Message::text("Your turn")).unwrap();
                 }
                 else {
-                    game.player2.as_ref().unwrap().send(Message::text("Your turn")).unwrap();
                     println!("Player 2 moved!");
+                    game.player1.as_ref().unwrap().send(Message::text("Your turn")).unwrap();
                 }
+
+                game.player1_turn = !game.player1_turn;
+    
+                send_game_state(&game, &game.player1.as_ref().unwrap(), game.player1_turn);
+                send_game_state(&game, &game.player2.as_ref().unwrap(), !game.player1_turn);
+    
             }
             drop(game);
             Ok(())
