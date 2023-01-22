5de164509f4fd4d7b4a0c9fd26c1d8550eff6a25
Kacper Klassa <kacperklassa@gmail.com>
2023-01-22 14:24:12 +0100

    add more game join tests

--- rust_go-server/src/tests.rs
+++ rust_go-server/src/tests.rs
@@ -1,6 +1,6 @@
 use ws::{Message, Result, CloseCode};
 use serde_json::{json};
-use std::thread;
+use std::{thread, time};
 use super::*;
 
 
@@ -28,6 +28,121 @@ fn test_player_one_join_game() -> Result<()> {
             out.send(json!({
                 "message_type": "stop_server"
             }).to_string()).unwrap();
+            Ok(())
+        }
+    }).unwrap();
+
+    Ok(())
+}
+
+#[test]
+fn test_two_players_join_game() -> Result<()> {
+
+    thread::spawn(move || {
+        start_server("127.0.0.1:8002");
+    });
+    
+    ws::connect("ws://127.0.0.1:8002", |out| {
+        let data = json!({
+            "message_type": "join_game",
+        });
+        out.send(Message::Text(data.to_string())).unwrap();
+
+        move |msg| {
+            let expected_data = json!({
+                "message_type": "join_game",
+                "status": "success",
+                "player": "first"
+            });
+            assert_eq!(msg, Message::Text(expected_data.to_string()));
+            out.close(CloseCode::Normal).unwrap();
+            Ok(())
+        }
+    }).unwrap();
+
+    thread::sleep(time::Duration::new(1, 0));
+
+    ws::connect("ws://127.0.0.1:8002", |out| {
+        let data = json!({
+            "message_type": "join_game",
+        });
+        out.send(Message::Text(data.to_string())).unwrap();
+
+        move |msg| {
+            let expected_data = json!({
+                "message_type": "join_game",
+                "status": "success",
+                "player": "second"
+            });
+            assert_eq!(msg, Message::Text(expected_data.to_string()));
+            out.close(CloseCode::Normal).unwrap();
+            Ok(())
+        }
+    }).unwrap();
+
+    Ok(())
+}
+
+#[test]
+fn test_third_player_tries_to_join_game() -> Result<()> {
+
+    thread::spawn(move || {
+        start_server("127.0.0.1:8003");
+    });
+    
+    ws::connect("ws://127.0.0.1:8003", |out| {
+        let data = json!({
+            "message_type": "join_game",
+        });
+        out.send(Message::Text(data.to_string())).unwrap();
+
+        move |msg| {
+            let expected_data = json!({
+                "message_type": "join_game",
+                "status": "success",
+                "player": "first"
+            });
+            assert_eq!(msg, Message::Text(expected_data.to_string()));
+            out.close(CloseCode::Normal).unwrap();
+            Ok(())
+        }
+    }).unwrap();
+
+    thread::sleep(time::Duration::new(1, 0));
+
+    ws::connect("ws://127.0.0.1:8003", |out| {
+        let data = json!({
+            "message_type": "join_game",
+        });
+        out.send(Message::Text(data.to_string())).unwrap();
+
+        move |msg| {
+            let expected_data = json!({
+                "message_type": "join_game",
+                "status": "success",
+                "player": "second"
+            });
+            assert_eq!(msg, Message::Text(expected_data.to_string()));
+            out.close(CloseCode::Normal).unwrap();
+            Ok(())
+        }
+    }).unwrap();
+
+    thread::sleep(time::Duration::new(1, 0));
+
+    ws::connect("ws://127.0.0.1:8003", |out| {
+        let data = json!({
+            "message_type": "join_game",
+        });
+        out.send(Message::Text(data.to_string())).unwrap();
+
+        move |msg| {
+            let expected_data = json!({
+                "message_type": "join_game",
+                "status": "error"
+            });
+            assert_eq!(msg, Message::Text(expected_data.to_string()));
+            out.close(CloseCode::Normal).unwrap();
             Ok(())
         }
     }).unwrap();
